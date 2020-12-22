6765770d7637d7a1f5446f9f6e3077f3597276bf
gopalkaran <16031024+gopalkaran@users.noreply.github.com>
2020-12-22 23:12:53 +0530

    email id as well as pass validation is added

--- script.js
+++ script.js
@@ -8,11 +8,19 @@ loginElement.addEventListener('click',validateEmail);
 
 function validateEmail(){
     var email_input=emailElement.value;
+    var pass_input=passElement.value;
     var regx=  /^[.\w]+@([\w-]+\.)+[\w]+$/; 
-    if(regx.test(email_input)){
+    // var reg = /^[\w]{6,}$/;
+    if(regx.test(email_input) && pass_input.match(/[a-z]/g) && pass_input.match( 
+        /[A-Z]/g) && pass_input.match( 
+        /[0-9]/g) && pass_input.match( 
+        /[^a-zA-Z\d]/g) && pass_input.length >= 6){
         alert('valid');
     }
     else{
         alert('invalid');
     }
+  
+    
+
 }
